dd0a8015781af84e04d8fc6faa0f0145a9665a37
hamed <hamed.alizadegan@gmail.com>
2025-04-20 07:18:52 +0330

    feat: partner page

--- static/assets/js/custom.js
+++ static/assets/js/custom.js
@@ -1,17 +1,24 @@
-(function ( window, document, $, undefined ) {
-	"use strict";
+(function (window, document, $, undefined) {
+  "use strict";
 
-	/* Enter your custom scripts */
+  /* Enter your custom scripts */
 
-	// اضافه کردن کلاس active به لینک فعلی در منو
-	$(document).ready(function() {
-		var currentLocation = window.location.pathname;
-		$('.c-header-1-nav a').each(function() {
-			var linkLocation = $(this).attr('href');
-			if (currentLocation.indexOf(linkLocation) !== -1) {
-				$(this).addClass('active');
-			}
-		});
-	});
+  // اضافه کردن کلاس active به لینک فعلی در منو
+  $(document).ready(function () {
+    var currentLocation = window.location.pathname;
+    $(".c-header-1-nav a").each(function () {
+      var linkLocation = $(this).attr("href");
+      if (currentLocation.indexOf(linkLocation) !== -1) {
+        $(this).addClass("active");
+      }
+    });
 
-})( window, document, jQuery );
+    // Fix for Contact us link on mobile
+    $(".c-header-1-nav li.mega > a").on("click", function (e) {
+      if ($(window).width() < 992) {
+        // Allow the link to work normally on mobile
+        return true;
+      }
+    });
+  });
+})(window, document, jQuery);
