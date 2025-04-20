5bc72d30a3bd89f7a65eed846e6f7564ebf44722
hamed <hamed.alizadegan@gmail.com>
2025-04-20 07:13:44 +0330

    feat: partner page

--- static/assets/js/custom.js
+++ static/assets/js/custom.js
@@ -3,4 +3,15 @@
 
 	/* Enter your custom scripts */
 
+	// اضافه کردن کلاس active به لینک فعلی در منو
+	$(document).ready(function() {
+		var currentLocation = window.location.pathname;
+		$('.c-header-1-nav a').each(function() {
+			var linkLocation = $(this).attr('href');
+			if (currentLocation.indexOf(linkLocation) !== -1) {
+				$(this).addClass('active');
+			}
+		});
+	});
+
 })( window, document, jQuery );
