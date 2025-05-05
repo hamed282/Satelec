017e54e85a61351f462c3712835a0c97f612caa3
hamed <hamed.alizadegan@gmail.com>
2025-05-05 07:33:13 +0330

    feat: site map

--- A/urls.py
+++ A/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from home.views import SitemapView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +10,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('blog/', include('blog.urls', namespace='blog')),
     path('tinymce/', include('tinymce.urls')),
+    path('sitemap.xml', SitemapView.as_view(), name='sitemap'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
